5f5b5f35ea6d9460ebfa55e9aeeeb2ef46b76105
Piotr Roslaniec <p.roslaniec@gmail.com>
2022-11-12 11:28:16 +0100

    refactor u in ciphertext into commitment

--- tpke/src/ciphertext.rs
+++ tpke/src/ciphertext.rs
@@ -11,9 +11,9 @@ use crate::{construct_tag_hash, hash_to_g2};
 
 #[derive(Clone, Debug)]
 pub struct Ciphertext<E: PairingEngine> {
-    pub nonce: E::G1Affine,    // U
-    pub ciphertext: Vec<u8>,   // V
-    pub auth_tag: E::G2Affine, // W
+    pub commitment: E::G1Affine, // U
+    pub ciphertext: Vec<u8>,     // V
+    pub auth_tag: E::G2Affine,   // W
 }
 
 impl<E: PairingEngine> Ciphertext<E> {
@@ -21,13 +21,13 @@ impl<E: PairingEngine> Ciphertext<E> {
         let hash_g2 = E::G2Prepared::from(self.construct_tag_hash());
 
         E::product_of_pairings(&[
-            (E::G1Prepared::from(self.nonce), hash_g2),
+            (E::G1Prepared::from(self.commitment), hash_g2),
             (g_inv.clone(), E::G2Prepared::from(self.auth_tag)),
         ]) == E::Fqk::one()
     }
     fn construct_tag_hash(&self) -> E::G2Affine {
         let mut hash_input = Vec::<u8>::new();
-        self.nonce.write(&mut hash_input).unwrap();
+        self.commitment.write(&mut hash_input).unwrap();
         hash_input.extend_from_slice(&self.ciphertext);
 
         hash_to_g2(&hash_input)
@@ -51,18 +51,18 @@ pub fn encrypt<R: RngCore, E: PairingEngine>(
     // s
     let product = E::product_of_pairings(&[(ry_prep, h_gen.into())]);
     // u
-    let blinded = g_gen.mul(rand_element).into();
+    let commitment = g_gen.mul(rand_element).into();
 
     let cipher = shared_secret_to_chacha::<E>(&product);
-    let nonce = blinded_to_nonce::<E>(blinded);
+    let nonce = commitment_to_nonce::<E>(commitment);
     let ciphertext = cipher.encrypt(&nonce, message).unwrap();
     // w
-    let auth_tag = construct_tag_hash::<E>(blinded, &ciphertext, aad)
+    let auth_tag = construct_tag_hash::<E>(commitment, &ciphertext, aad)
         .mul(rand_element)
         .into();
 
     Ciphertext::<E> {
-        nonce: blinded,
+        commitment,
         ciphertext,
         auth_tag,
     }
@@ -74,13 +74,13 @@ pub fn check_ciphertext_validity<E: PairingEngine>(
 ) -> bool {
     let g_inv = E::G1Prepared::from(-E::G1Affine::prime_subgroup_generator());
     let hash_g2 = E::G2Prepared::from(construct_tag_hash::<E>(
-        c.nonce,
+        c.commitment,
         &c.ciphertext[..],
         aad,
     ));
 
     E::product_of_pairings(&[
-        (E::G1Prepared::from(c.nonce), hash_g2),
+        (E::G1Prepared::from(c.commitment), hash_g2),
         (g_inv, E::G2Prepared::from(c.auth_tag)),
     ]) == E::Fqk::one()
 }
@@ -90,7 +90,7 @@ pub fn decrypt<E: PairingEngine>(
     privkey: E::G2Affine,
 ) -> Vec<u8> {
     let s = E::product_of_pairings(&[(
-        E::G1Prepared::from(ciphertext.nonce),
+        E::G1Prepared::from(ciphertext.commitment),
         E::G2Prepared::from(privkey),
     )]);
     decrypt_with_shared_secret(ciphertext, &s)
@@ -100,7 +100,7 @@ pub fn decrypt_with_shared_secret<E: PairingEngine>(
     ciphertext: &Ciphertext<E>,
     s: &E::Fqk,
 ) -> Vec<u8> {
-    let nonce = blinded_to_nonce::<E>(ciphertext.nonce);
+    let nonce = commitment_to_nonce::<E>(ciphertext.commitment);
     let ciphertext = ciphertext.ciphertext.to_vec();
 
     let cipher = shared_secret_to_chacha::<E>(s);
@@ -120,23 +120,27 @@ pub fn checked_decrypt_with_shared_secret<E: PairingEngine>(
     Some(decrypt_with_shared_secret(ciphertext, s))
 }
 
+fn blake2s_hash(input: &[u8]) -> Vec<u8> {
+    let mut hasher = blake2b_simd::Params::new().hash_length(32).to_state();
+    hasher.update(input);
+    hasher.finalize().as_bytes().to_vec()
+}
+
 pub fn shared_secret_to_chacha<E: PairingEngine>(
     s: &E::Fqk,
 ) -> ChaCha20Poly1305 {
     let mut prf_key = Vec::new();
     s.write(&mut prf_key).unwrap();
-    let mut blake_params = blake2b_simd::Params::new();
-    blake_params.hash_length(32);
-    let mut hasher = blake_params.to_state();
-    prf_key.write(&mut hasher).unwrap();
-    let mut prf_key_32 = [0u8; 32];
-    prf_key_32.clone_from_slice(hasher.finalize().as_bytes());
+    let prf_key_32 = blake2s_hash(&prf_key);
 
     ChaCha20Poly1305::new(GenericArray::from_slice(&prf_key_32))
 }
 
-fn blinded_to_nonce<E: PairingEngine>(nonce: E::G1Affine) -> Nonce {
-    let mut nonce_bytes = Vec::new();
-    nonce.serialize_unchecked(&mut nonce_bytes).unwrap();
-    *Nonce::from_slice(&nonce_bytes[..12])
+fn commitment_to_nonce<E: PairingEngine>(commitment: E::G1Affine) -> Nonce {
+    let mut commitment_bytes = Vec::new();
+    commitment
+        .serialize_unchecked(&mut commitment_bytes)
+        .unwrap();
+    let commitment_hash = blake2s_hash(&commitment_bytes);
+    *Nonce::from_slice(&commitment_hash[..12])
 }
